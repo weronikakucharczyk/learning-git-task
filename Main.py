78cd62aa86f93ab775c4ab2a1a01997a21c7d9e1
unknown <weronika_kucharczyk@outlook.com>
2026-03-12 13:27:11 +0100

    Add dictionary and loop

---
+++ Main.py
@@ -0,0 +1,6 @@
+lista_zakupow={
+    "piekarnia": ["chleb", "pączek", "bułki"],
+    "warzywniak": ["marchew", "seler", "rukola"]
+}
+for sklep, produkty in lista_zakupow.items():
+    print( f"Idę do {sklep} i kupuję tam {produkty}")
