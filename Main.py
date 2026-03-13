b4c9a901d41ed20a277cf9392e25c9c923981728
unknown <weronika_kucharczyk@outlook.com>
2026-03-13 09:15:32 +0100

    add products for pancakes

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 lista_zakupow={
     "piekarnia": ["chleb", "pączek", "bułki", "jagodzianki", "cynamonki"],
-    "warzywniak": ["marchew", "seler", "rukola", "sałata", "pomidory"]
+    "warzywniak": ["marchew", "seler", "rukola", "sałata", "pomidory"],
+    "supermarket": ["mąka", "jajka", "mleko", "woda", "olej"]
 }
 
 suma_prod = 0
@@ -11,5 +12,8 @@ for sklep, produkty in lista_zakupow.items():
     print( f"Idę do {sklep_wielka_lit} i kupuję tam {produkty_wielka_lit}")
 
     suma_prod += len(produkty)
+prod_supermkt = lista_zakupow["supermarket"]
+produkty_str = ", ".join(prod_supermkt)
 
 print(f"W sumie kupuję {suma_prod} produktów.")
+print(f"Z produktów z supermarketu: ({produkty_str}) zrobię naleśniki")
